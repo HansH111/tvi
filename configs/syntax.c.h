0a3fbed8c857572d2499e5060660673edfb27548
Hans Harder <hans@atbas.org>
2026-02-04 15:55:31 +0100

    change colors and added size_t| as kw2

--- configs/syntax.c.h
+++ configs/syntax.c.h
@@ -1,16 +1,16 @@
 # === C syntax highlighting ===
 #
 # --- 256-color codes for: normal, kw1, kw2, comment/mlcomment, string, number, match, notprintable
-colors256  : 249 117 48 73 73 246 246 195 196
+colors256  : 250 117 35 242 73 244 166 196
 #
 # --- control flow keywords (=kw1)
 keywords  : if else do while for break continue return goto switch case default
 #
 # --- type keywords with | at the end (=kw2)
-keywords   : int| long| double| float| char| unsigned| signed| void| short| 
+keywords   : int| long| double| float| char| unsigned| signed| void| short|
 keywords   : struct| union| typedef| static| enum| cons|t volatile| auto| register|
 keywords   : sizeof| typeof| class| public| private| protected| virtual| override|
-keywords   : true| false| bool|
+keywords   : true| false| bool| size_t|
 #
 # --- single-line comment
 remark     : //
